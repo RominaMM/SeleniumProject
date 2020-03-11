22ec71e5bea150258cd39e21587584cece7c1c95
romina <rominamihaila@gmail.com>
2020-03-11 11:12:15 +0200

    SeleniumProjectTema

--- java/Main.java
+++ java/Main.java
@@ -7,39 +7,35 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //  login();
-        // register();
+        login();
+        register();
         wishList();
+    }
+    public static void login() {
 
-
+    System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+    WebDriver driver = new ChromeDriver();
+    driver.get("http://testfasttrackit.info/selenium-test/");
+    driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
+    driver.findElement(By.cssSelector("#header-account > div > ul > li.last > a")).click();
+    driver.findElement(By.cssSelector("#email")).sendKeys("cosmin@fasttrackit.org");
+    driver.findElement(By.cssSelector("#pass")).sendKeys("123456");
+    driver.findElement(By.cssSelector("#send2")).click();
+    driver.quit();
     }
 
-    //  public static void login() {
+    public static void register() {
 
-    //  System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
-    //   WebDriver driver = new ChromeDriver();
-//
-    //  driver.get("http://testfasttrackit.info/selenium-test/");
-    //  driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
-    //  driver.findElement(By.cssSelector("#header-account > div > ul > li.last > a")).click();
-    //   driver.findElement(By.cssSelector("#email")).sendKeys("cosmin@fasttrackit.org");
-    // driver.findElement(By.cssSelector("#pass")).sendKeys("123456");
-    //  driver.findElement(By.cssSelector("#send2")).click();
-    //   driver.quit();
-    // }
+     System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+     WebDriver driver = new ChromeDriver();
 
-    // public static void register() {
+     driver.get("http://testfasttrackit.info/selenium-test/");
+    driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
+     driver.findElement(By.cssSelector("#header-account > div > ul > li.last > a")).click();
+     driver.findElement(By.cssSelector("#send2")).click();
+     driver.quit();
 
-    //  System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
-    //  WebDriver driver = new ChromeDriver();
-
-    //  driver.get("http://testfasttrackit.info/selenium-test/");
-    //  driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
-    //  driver.findElement(By.cssSelector("#header-account > div > ul > li.last > a")).click();
-    // driver.findElement(By.cssSelector("#send2")).click();
-    //  driver.quit();
-
-    // }
+     }
     public static void wishList() {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
